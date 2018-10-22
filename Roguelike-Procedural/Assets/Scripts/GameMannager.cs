578f4dda40f0ab49b0e7cb15118924d5ab640806
zed15600 <zed15600@gmail.com>
2018-10-22 18:20:01 -0500

    Adding board and game managmente

---
+++ Roguelike-Procedural/Assets/Scripts/GameMannager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMannager : MonoBehaviour {
+
+    public BoardMannager boardScript;
+
+    private int level = 3;
+
+	// Use this for initialization
+	void Awake () {
+		boardScript = GetComponent<BoardMannager>();
+        initGame();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void initGame() {
+        boardScript.SetupScene(level);
+    }
+}
